92510d593b1c57ba7cc45c4024f5c07f0150c919
Sage <sage@sagenite.net>
2013-01-22 12:06:58 -0800

    Token parsing works

--- pissed.php
+++ pissed.php
@@ -1,34 +1,56 @@
 <?php
 
-define('TOKEN_PATTERN', '/^(\(|\)|[^\s"]+|"([^"]|\\")*")$/');
+define('TOKEN_PATTERN',  '/^(\(|\)|"([^"]|\\")*|[a-z]+)$/s');
+define('TOKEN_COMPLETE', '/^(\(|\)|"([^"]|\\")*")$/m');
 
 //Prefix: There can be any ammount of whitespace at the beginning of the token.
 //Single char tokens: These can only be 1 char long ( )
 //If the first char is a " the token lasts until the another " is found that isn't preceded by \
 //Otherwise the token lasts until a whitespace character or ( or ) is found
 
+class BufferedStream{
+  var $stream;
+  var $buffer;
+
+  function __construct($stream){
+    $this->stream = $stream;
+    $this->buffer = Array();
+  }
+
+  function getc(){
+    return ((sizeof($this->buffer) > 0) ?
+            array_pop($this->buffer) : fgetc($this->stream));
+  }
+
+  function putc($char){array_unshift($this->buffer,$char);}
+}
 
 function is_token($token){
-  return preg_match(TOKEN_PATTERN, $token);
+  return (preg_match(TOKEN_PATTERN, $token) and !preg_match('/[\n\r]$/',$token));
 }
 
-function test_token($token){
-  print $token.": ".(is_token($token) ? "yes" : "no")."\n";
+function peel_whitespace($bstream){
+  $byte = $bstream->getc();
+  if($byte === false){ return;}
+  elseif(preg_match('/\s/',"$byte")){ peel_whitespace($bstream);}
+  else{$bstream->putc($byte);}
 }
 
-foreach(Array('',
-              '(',
-              '( ',
-              ')',
-              ') ',
-              'cow',
-              'cow ',
-              '"cow',
-              '"cow"',
-              '"cow"ssd',
-              '"cows\" and more"',
-              '"cows\" and more') as $token){
-  test_token($token);
+function read_token($bstream){
+  $buffer = '';
+  $first = true;
+  peel_whitespace($bstream);
+  do{
+    $buffer .= $bstream->getc();
+    if(preg_match(TOKEN_COMPLETE, $buffer)){return $buffer;}
+  } while(is_token($buffer));
+
+  $bstream->putc(substr($buffer,-1));
+  return substr($buffer,0,-1);
 }
 
+$input = new BufferedStream(fopen('php://stdin','r'));
+print("Token: '".read_token($input)."'\n");
+print("Token: '".read_token($input)."'\n");
+print("Token: '".read_token($input)."'\n");
 ?>
